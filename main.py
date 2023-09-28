2dc1f25d42a82af0c49cca9cc612d1892818c474
crafty-arl <126638250+crafty-arl@users.noreply.github.com>
2023-09-28 17:09:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ HEADERS = {
 }
 
 # Initialize Firebase
-service_account_info = json.loads(st.secrets["FIREBASE_CRED"])
+service_account_info = json.loads(FIREBASE_CRED)
+service_account_info['private_key'] = service_account_info['private_key'].encode('utf-8').decode('unicode_escape')
 credentials = Credentials.from_service_account_info(service_account_info)
 db = firestore.Client(credentials=credentials)
 # Functions for Prodia API
